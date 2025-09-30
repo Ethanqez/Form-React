cff2c4a5c224b2c2cb33b7965e5260c929a9bd30
fjexx <fjessiel.ord26@gmail.com>
2025-09-30 14:10:31 -0500

    Levantar BD con Docker Compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import db from './db.js';
+import mongoose from 'mongoose';
 import routerIndex from './routers/index.router.js';
 import cors from 'cors';
 
@@ -11,19 +11,27 @@ import cors from 'cors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-db();
+//Conexión a MongoDB
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/marcaciones';
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB conectado'))
+.catch(err => console.error('Error conectando MongoDB', err));
+// --------------------------------------------------------
+
 const app = express();
 
 app.use(cors());
-
-// Middleware for parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the public directory
+// Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Set Content Security Policy headers
+// Content Security Policy
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
@@ -32,15 +40,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// Use the router for API endpoints
+// API routes
 app.use('/api', routerIndex);
 
-// Serve the HTML file for the root route
+// Serve index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
+  console.log(`Server started at ${PORT}`);
 });
