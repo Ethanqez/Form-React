80760269b5941343ae36076395ffc7da2e6294ca
Ethanqez <izanmonrroy309@gmail.com>
2025-09-30 16:30:12 -0500

    Fail 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,31 @@ import { fileURLToPath } from 'url';
 import db from './db.js';
 import routerIndex from './routers/index.router.js';
 import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = "mongodb+srv://admin:20232212pass@cluster0.pjjcrkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 // Get directory name in ES modules
